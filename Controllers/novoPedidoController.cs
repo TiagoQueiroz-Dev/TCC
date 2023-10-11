624bbfb6c95486261fd7438f188371c0ca103bfb
Condorruffles <65664248+Condorruffles@users.noreply.github.com>
2023-10-11 08:23:04 -0300

    .

---
+++ Controllers/novoPedidoController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TCC.Models;
+
+namespace TCC.Controllers;
+
+public class novoPedidoController : Controller
+{
+    private readonly ILogger<novoPedidoController> _logger;
+
+    public novoPedidoController(ILogger<novoPedidoController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult novoPedido()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
