9a12cf4ce843158a132b6c87516a9f654cb95bcb
Kevin Céssar <102882654+KevinCesarDev@users.noreply.github.com>
2023-11-23 01:58:02 -0300

    .

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,19 +1,52 @@
-﻿function atualizarQuantidade(p,q)  {
-    var inputQtd = document.getElementById(p);
-    var qtdProduto = document.getElementById(q);
+﻿
+function calcularValorTotal(p,q,r,s) {
+    calcularValorParcial(p,q,r);
+    somarTotal(r,s);
     
-    qtdProduto.textContent = inputQtd.value;
-    console.log(qtdProduto.textContent);
+    // console.log(document.getElementById(r).innerText)
+}
+
+function calcularValorParcial(p, q, r) {
+
+    var unidadeSemVirgula = (document.getElementById(p).innerText).toString();
+    var valorUnidade = unidadeSemVirgula.replace(',', '.');
+    valorUnidade = parseFloat(valorUnidade) || 0;
+
+    //console.log(valorUnidade);
+
+    var inputQtd = parseFloat(document.getElementById(q).value) || 0;
+
+    //console.log(inputQtd);
+
+    var valorParcial = parseFloat(document.getElementById(r).value) || 0;
+    //console.log(valorParcial);
+
+    valorParcial += valorUnidade * inputQtd;
+
+    //valor passado em formato de string porém com . ao inves de , para facilitar o calculo total posteriormente
+    document.getElementById(r).innerText = valorParcial.toString();
 };
 
-function calcularValorProdutos(p){
+function somarTotal(r,s) {
+    var valorParcial = document.getElementById(r).innerText|| 0;
+    valorParcial = parseFloat(valorParcial);
 
-    var unidadeSemVirgula = p.toString();
-    var valorUnidade = unidadeSemVirgula.replace(',','.');
+    console.log(valorParcial)
 
-    console.log(valorUnidade);
+    var valorTotal = (document.getElementById(s).innerText).replace(',','.');
+    valorTotal = parseFloat(valorTotal);
+    console.log(valorTotal);
+   
+    //var valorTotal = parseFloat(totalComVirgula);
+    //console.log(typeof valorTotal)
     
     
+    
+    valorTotal += valorParcial;    
+   
 
-    // document.getElementById(i).innerText = 'Total = R$ '+ parseFloat(valorTotal);
-};
+
+    document.getElementById(s).innerText = valorTotal.toString().replace('.',',');
+
+
+}
