3cc66be2cadb80f98998797394f8ce096e0412c5
Kevin Céssar <102882654+KevinCesarDev@users.noreply.github.com>
2023-11-23 17:16:52 -0300

    .

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,8 @@
 ﻿
-function calcularValorTotal(p,q,r,s) {
-    calcularValorParcial(p,q,r);
-    somarTotal(r,s);
-    
-    // console.log(document.getElementById(r).innerText)
+function calcularValorTotal(p, q, r, s, size) {
+    calcularValorParcial(p, q, r);
+    atribuirTotal(s, size);
+
 }
 
 function calcularValorParcial(p, q, r) {
@@ -12,41 +11,31 @@ function calcularValorParcial(p, q, r) {
     var valorUnidade = unidadeSemVirgula.replace(',', '.');
     valorUnidade = parseFloat(valorUnidade) || 0;
 
-    //console.log(valorUnidade);
 
     var inputQtd = parseFloat(document.getElementById(q).value) || 0;
 
-    //console.log(inputQtd);
 
-    var valorParcial = parseFloat(document.getElementById(r).value) || 0;
-    //console.log(valorParcial);
+    var valorParcial = parseFloat(document.getElementById(r).innerText) || 0;
 
-    valorParcial += valorUnidade * inputQtd;
+    valorParcial = (valorUnidade*inputQtd).toFixed(2);
+    console.log(valorParcial);
 
-    //valor passado em formato de string porém com . ao inves de , para facilitar o calculo total posteriormente
     document.getElementById(r).innerText = valorParcial.toString();
 };
 
-function somarTotal(r,s) {
-    var valorParcial = document.getElementById(r).innerText|| 0;
-    valorParcial = parseFloat(valorParcial);
 
-    console.log(valorParcial)
+function atribuirTotal(s, size) {
 
-    var valorTotal = (document.getElementById(s).innerText).replace(',','.');
-    valorTotal = parseFloat(valorTotal);
-    console.log(valorTotal);
-   
-    //var valorTotal = parseFloat(totalComVirgula);
-    //console.log(typeof valorTotal)
-    
-    
-    
-    valorTotal += valorParcial;    
-   
+    var valorTotal = parseFloat(valorTotal);
+    valorTotal = 0;
 
+    for (var i = 1; i <= size; i++) {
+        i = i.toString();
+        var valorParcial = document.getElementById(i + "-lista").innerText || 0;
+        valorParcial = parseFloat(valorParcial);
+        valorTotal += valorParcial;
+    }
+    valorTotal = valorTotal.toFixed(2)
 
-    document.getElementById(s).innerText = valorTotal.toString().replace('.',',');
-
-
+    document.getElementById(s).innerText = valorTotal.toString().replace('.', ',');
 }
